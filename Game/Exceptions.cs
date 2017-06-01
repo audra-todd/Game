8274e57e75dee59310b57697ea7cf84314e31af8
Audra Todd <audra_todd@hotmail.com>
2017-05-31 22:02:48 -0400

    Create a tower defense game using C#.  Plot points.

---
+++ Game/Exceptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Game
+{
+    class GameException : System.Exception
+    {
+        public GameException()
+        {
+            
+        }
+
+        public GameException(string message) : base(message)
+        {
+
+        }
+    }
+
+    class OutOfBoundsException : GameException
+    {
+        public OutOfBoundsException()
+        {
+            
+        }
+
+        public OutOfBoundsException(string message) : base(message)
+        {
+            
+        }
+    }
+}
---
+++ Game/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Game
+{
+	class Game
+	{
+		public static void Main()
+		{
+            Map map = new Map(8, 5);
+
+            try
+            {
+                MapLocation mapLocation = new MapLocation(20, 20, map);
+            }
+            catch(OutOfBoundsException ex)
+            {
+                Console.WriteLine(ex.Message);   
+            }
+            catch(GameException ex)
+            {
+                Console.WriteLine("Unhandled GameException");
+            }
+
+            catch(Exception)
+            {
+                Console.WriteLine("Unhandled Exception");
+            }
+		}
+	}
+}
